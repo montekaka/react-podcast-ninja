465bd2ebd37e0bee1e765ecb72559694f3907788
montekaka <josh.chen.jia@gmail.com>
2021-08-13 11:41:26 -0700

    adds bouncing to indicate loading

---
+++ src/components/ninja-player/LoadingBouncing.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './loading-bouncing.css';
+
+const LoadingBouncing = () => {
+  return (
+    <div className="bouncer">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>      
+    </div>
+  )
+}
+
+export default LoadingBouncing;
