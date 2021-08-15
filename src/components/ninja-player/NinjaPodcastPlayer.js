f66204629e6dd891e051657f427aeef3d436ccfe
montekaka <josh.chen.jia@gmail.com>
2021-08-14 21:55:34 -0700

    removes unnecssary code

--- src/components/ninja-player/NinjaPodcastPlayer.js
+++ src/components/ninja-player/NinjaPodcastPlayer.js
@@ -3,8 +3,6 @@ import NinjaPlayer from './NinjaPlayer'
 import LoadingScreenContainer from './LoadingScreenContainer'
 import {useFetchRss} from './hooks'
 import LoadingBouncing from './LoadingBouncing'
-import { useAtom } from 'jotai';
-import {chaptersAtom} from './jotai'
 
 const NinjaPodcastPlayer = ({
   rssFeedUrl, playerId, configs, proxy,singleEpisode
@@ -12,11 +10,6 @@ const NinjaPodcastPlayer = ({
 
   // const [episodes, setEpisodes] = useState([]);
   const [episodes, loading, errorMessage] = useFetchRss(rssFeedUrl, proxy)
-  const [, setChapters] = useAtom(chaptersAtom)
-
-  useEffect(() =>{ 
-    setChapters([]);
-  }, [rssFeedUrl])
 
   if(loading) {
     return (
