6a5f6e9e7ed834d9e6c4b7f7796244b56798f8d0
montekaka <josh.chen.jia@gmail.com>
2021-08-22 23:29:46 -0700

    adds style to play-pause button

--- src/components/discast/PlayPauseButton.js
+++ src/components/discast/PlayPauseButton.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import {useAtom} from 'jotai'
+import { Play, Pause } from 'react-feather';
 import {
   enclosureUrlAtom,
   playerAtom,
+  configsAtom,
   togglePlayPauseAtom
 } from './jotai'
 
 const PlayPuaseButton = () => {
   const [playerState, togglePlayPause] = useAtom(togglePlayPauseAtom);
+  const {playing} = playerState;
+  const [playerSkin] = useAtom(configsAtom);
 
   return (
-    <div onClick={togglePlayPause} className="bh-main-play-pause-button">
-      Play
+    <div onClick={togglePlayPause} 
+      className="bh-main-play-pause-button"
+      style={{background: playerSkin.primaryButtonColor}}
+    >
+      {playing ? <Pause style={{color: playerSkin.primaryBackgroundColor}} className="icon"/> : <Play style={{color: playerSkin.primaryBackgroundColor}} className="icon"/>}
     </div>
   )  
 
