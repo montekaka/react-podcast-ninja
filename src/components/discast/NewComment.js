0097e33f88e5b2b0f08d1604f3b3329101adbc17
montekaka <josh.chen.jia@gmail.com>
2021-08-24 23:50:54 -0700

    adds style to mini player

--- src/components/discast/NewComment.js
+++ src/components/discast/NewComment.js
@@ -3,6 +3,7 @@ import {useAtom} from "jotai"
 import ProgressControlSlider from './ProgressControlSlider'
 import PlayPauseButton from './PlayPauseButton'
 import Artwork from './Artwork'
+import {getHHMMSSFromSeconds} from './libs'
 import {
   configsAtom,
   playerAtom,
@@ -13,10 +14,12 @@ import {
 
 const NewComment = () => {
   const [metaState] = useAtom(metasAtom);
+  const [playerState] = useAtom(playerAtom);
+  const {durationSeconds, playedSeconds} = playerState;
 
   return (
     <div>
-      <div>
+      <div style={{display: 'flex'}}>
         <Artwork src={metaState.src} style={{
           width: "40px",
           height: "40px",
@@ -32,11 +35,28 @@ const NewComment = () => {
             size="12"
           />
         </Artwork>
-        <ProgressControlSlider/>
+        <div style={{
+          height: '40px',
+          width: "100%", 
+          marginLeft: '10px',
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'flex-end'
+        }}>
+          <div style={{
+            display: 'flex',
+            fontSize: "10px",
+            justifyContent: 'space-between'
+          }}>
+            <div>{getHHMMSSFromSeconds(playedSeconds)}</div>
+            <div>{getHHMMSSFromSeconds(durationSeconds)}</div>
+          </div>
+          <ProgressControlSlider/>
+        </div>        
       </div>
       <div>
         <input placeholder="00:00"/>
-        <input placeholder="00:00"/>        
+        <input placeholder="00:00"/>
       </div>
       <div>
         <textarea type="textarea" rows="5" 
