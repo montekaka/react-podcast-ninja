4cdfb40f57bf74611b2f609b73bb33f7280ec054
montekaka <josh.chen.jia@gmail.com>
2022-01-17 23:12:56 -0800

    adds default justcast-widget as user agent

--- src/components/ninja-player/libs/parser.js
+++ src/components/ninja-player/libs/parser.js
@@ -1,19 +1,21 @@
 import Parser from 'rss-parser';
 import axios from 'axios'
 
-const parser = new Parser({
-  customFields: {
-    item: [
-      ["podcast:chapters", "chapters"] // rename the filed to chapters
-    ]
-  }
-})
 
-
-export const feedParser = (rssFeed, proxy) => {
+export const feedParser = (rssFeed, proxy, userAgent) => {
   if(proxy && proxy.length > 0) {
     return axios.get(`${proxy}${rssFeed}`)
   } else {
+    const headers = {'User-Agent': userAgent ? userAgent : "JustCast-Widget"};
+
+    const parser = new Parser({
+      headers,
+      customFields: {
+        item: [
+          ["podcast:chapters", "chapters"] // rename the filed to chapters
+        ]
+      }
+    })    
     return parser.parseURL(rssFeed)
   }
 }
