bc6a6e711d8969aa6b9ad54529426bc6c3ae22ee
montekaka <josh.chen.jia@gmail.com>
2022-01-22 00:02:07 -0800

    adds css style to the a tag in the description

--- src/components/ninja-player/MoreInfoSection.js
+++ src/components/ninja-player/MoreInfoSection.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import styled from 'styled-components';
 import {useAtom} from "jotai"
 import {episodesAtom, playingIdAtom, playerSkinAtom, themeNameAtom} from './jotai'
 import MoreInfoSectionHeader from './MoreInfoSectionHeader'
@@ -9,6 +10,13 @@ const MoreInfoSection = () => {
   const [episodes] = useAtom(episodesAtom);
   const [playingId] = useAtom(playingIdAtom);
   const [themeName] = useAtom(themeNameAtom);
+  
+  const Description = styled.div`
+    a {
+      color: ${playerSkin.primaryTextColor};
+      opacity: 0.8;
+    }
+  `;  
   
   if(episodes.length > 0) {
     const {title, link, description} = episodes[playingId];
@@ -29,7 +37,7 @@ const MoreInfoSection = () => {
           {link ? <div className={`${themeName}-more-info-link`} style={{color: playerSkin.primaryTextColor}} onClick={openLink}>View the website</div> : null}
         </MoreInfoSectionHeader>
         <div className={`${themeName}-more-info-descripion`}>
-          {description ? <div dangerouslySetInnerHTML={{__html: description}}></div> : null}
+          {description ? <Description dangerouslySetInnerHTML={{__html: description}} style={{color: playerSkin.primaryTextColor, link}}></Description> : null}
         </div>
       </div>
     )
