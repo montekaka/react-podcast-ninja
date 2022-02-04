ffe8ce6c624c79b7b105407c5c6071c829019ffa
montekaka <josh.chen.jia@gmail.com>
2022-02-04 14:33:27 -0800

    implements pause function to play-pause click

--- src/components/pie-player/components/EpisodeListItem.js
+++ src/components/pie-player/components/EpisodeListItem.js
@@ -18,20 +18,28 @@ export default function EpisodeListItem(props) {
     duration
   } = props;
   const [playerState, setPlayerState] = useAtom(upatePlayerAtom);
-  const {muted} = playerState;
+  const {muted, playing} = playerState;
   
   const onClick = () => {
-    const payload = {url, playingId: idx, playing: true};
-    setPlayerState(payload);    
-    // What is going on here?
-    // because safari only allow autoplay with muted.
-    // so that we will tell the player to muted first, then unmuted it after 500 ms
-    // https://github.com/CookPete/react-player/issues/338#issuecomment-382022759
-    if(muted === true) {
-      setTimeout(() => {
-        setPlayerState({muted: false})
-      }, 500)
+    if(playing === true) {
+      // pause
+      const payload = {url: null, playingId: null, playing: false, onReady: false};
+      setPlayerState(payload);
+    } else {
+      // play
+      const payload = {url, playingId: idx, playing: true};
+      setPlayerState(payload);
+      // What is going on here?
+      // because safari only allow autoplay with muted.
+      // so that we will tell the player to muted first, then unmuted it after 500 ms
+      // https://github.com/CookPete/react-player/issues/338#issuecomment-382022759
+      if(muted === true) {
+        setTimeout(() => {
+          setPlayerState({muted: false})
+        }, 500)
+      }      
     }
+
   }
 
   return (
